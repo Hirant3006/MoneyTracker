df6bf420a016adbcc2922358a79bf673131bbcac
unknown <lychautrinha@gmail.com>
2019-04-06 09:21:05 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
